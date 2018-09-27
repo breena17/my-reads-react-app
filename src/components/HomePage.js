cec47a12a5867b5f1ac5b8de8cd3dae53e49e3b0
Breena Troy <breena17@gmail.com>
2018-09-26 22:07:18 -0700

    added components for books, shelfs, search, home

---
+++ src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Shelf from './Shelf'
+import * as BooksAPI from '../BooksAPI'
+
+class HomePage extends React.Component{
+    componentDidMount() {
+        BooksAPI.getAll()
+        .then((books) => {
+            console.log(books)
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Shelf/>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search">Add a Book</Link>
+            </div>
+          </div>
+        
+        );
+    }
+}
+export default HomePage
