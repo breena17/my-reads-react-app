c7f297db5ecd5f0ab4f24d9d817d1f848bb75e1f
Breena Troy <breena17@gmail.com>
2018-09-26 23:09:12 -0700

    connected to BooksAPI in Books and Shelf

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,10 +4,17 @@ import Shelf from './Shelf'
 import * as BooksAPI from '../BooksAPI'
 
 class HomePage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: []
+        }
+    }
     componentDidMount() {
         BooksAPI.getAll()
         .then((books) => {
             console.log(books)
+            this.setState({ books })
         })
     }
 
@@ -20,7 +27,9 @@ class HomePage extends React.Component{
             </div>
             <div className="list-books-content">
               <div>
-                <Shelf/>
+                <Shelf name="Currently Reading" books={this.state.books.filter(b => b.shelf === "currentlyReading")}/>
+                <Shelf name="Want To Read" books={this.state.books.filter(b => b.shelf === "wantToRead")}/>
+                <Shelf name="Read" books={this.state.books.filter(b => b.shelf === "read")}/>
               </div>
             </div>
             <div className="open-search">
